20a612b54064cf9d31c8f367b562d3b97455cd22
kostabkpark <kostabkpark@gmail.com>
2025-05-13 15:22:15 +0900

    jsp forward

--- src/main/java/ch05/ProductController.java
+++ src/main/java/ch05/ProductController.java
@@ -22,5 +22,23 @@ public class ProductController extends HttpServlet {
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String action = req.getParameter("action");
 		System.out.println(action);
+		String path="";
+		if(action == null) {
+			action = "list";
+		}
+		if(action.equals("list")) {
+			path = "/ch05/productList.jsp";
+			// 데이터는 service.findAll() 를 넘긴다.
+			req.setAttribute("plist", service.findAll());
+			// productList.jsp 화면 이동
+			req.getRequestDispatcher(path).forward(req, resp);
+		} else if(action.equals("info")) {
+			String pid = req.getParameter("pid");
+			// pid 을 매개변수로 하는 service.find(pid) 를 호출한 후 
+			req.setAttribute("p", service.find(pid));
+			path = "/ch05/productInfo.jsp";
+			// productInfo.jsp로 화면 이동
+			req.getRequestDispatcher(path).forward(req, resp);
+		}
 	}
 }
