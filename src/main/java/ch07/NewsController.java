967ea74b70470fe91fb73faa6482698404d088f3
kostabkpark <kostabkpark@gmail.com>
2025-05-15 15:54:46 +0900

    news - add

--- src/main/java/ch07/NewsController.java
+++ src/main/java/ch07/NewsController.java
@@ -7,15 +7,18 @@ import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 /**
  * Servlet implementation class NewsController
  */
 @WebServlet("/news")
+@MultipartConfig(maxFileSize = 1024*1024*2, location = "/img")
 public class NewsController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private NewsDAO newsDAO;
@@ -34,6 +37,7 @@ public class NewsController extends HttpServlet {
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
 		String method = req.getMethod();
 		String action = req.getParameter("action");
 		String path = "/ch07/";
@@ -57,6 +61,8 @@ public class NewsController extends HttpServlet {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				} break;
+			case "addNews" : view = addNews(req, resp); break;
+			case "delNews" : view = delNews(req, resp); break;
 			}
 			context.getRequestDispatcher(path + view).forward(req, resp);
 		}
@@ -72,5 +78,16 @@ public class NewsController extends HttpServlet {
 		News news = newsDAO.getNews(aid);
 		req.setAttribute("news", news);
 		return "newsView.jsp";
+	}
+	
+	private String addNews(HttpServletRequest req, HttpServletResponse resp)  {
+		News n = new News();
+		Part part = req.getPart("img");
+		return "newsList.jsp";
+	}
+	
+	private String delNews(HttpServletRequest req, HttpServletResponse resp)  {
+		
+		return "newsList.jsp";
 	}
 }
---
+++ src/main/webapp/ch07/newsAdd.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h2>뉴스 등록</h2>
+	<hr>
+	<form action="/news?action=addNews" method="post" enctype="multipart/form-data">
+		<label>뉴스 제목</label>
+		<input type="text" name="title">
+		<label>뉴스 내용</label>
+		<textarea rows="5" cols="20" name="content"></textarea>
+		<input type="file" name="img" accept="image/*">
+		<input type="submit" value="전송">
+	</form>
+</body>
+</html>
